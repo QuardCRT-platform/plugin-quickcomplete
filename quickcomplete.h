ae9be908e1e7ce0cbb8f0958aca420ff4114f9c9
xiaoming <2014500726@smail.xtu.edu.cn>
2024-07-24 13:45:30 +0800

    Update plugin version to 0.0.3
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- quickcomplete.h
+++ quickcomplete.h
@@ -4,7 +4,7 @@
 #include "plugininterface.h"
 
 #define PLUGIN_NAME    "Quick Complete"
-#define PLUGIN_VERSION "0.0.2"
+#define PLUGIN_VERSION "0.0.3"
 #define PLUGIN_WEBSITE "https://github.com/QuardCRT-platform/plugin-quickcomplete"
 
 class QuickComplete : public PluginInterface
